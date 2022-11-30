53c7c67ef5dca9a49994beb42e6a168899559abe
Bobby Tat <messaroundacc1@hotmail.com>
2022-11-30 05:43:37 -0800

    made some progress on the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # library project
 <!-- Next: work on changing console.log to return for the prototype function that reports the book information -->
+work on addBookToLibrary function
--- javascript.js
+++ javascript.js
@@ -1,14 +1,27 @@
+let myLibrary = [];
+
 function Book(title, author, pages, bookRead) {
     this.title = title
     this.author = author
     this.pages = pages
     this.bookRead = bookRead
-    this.sayBookinfo = function() {
+    this.info = function() {
         // console.log(title+', by '+author+', '+pages+' pages, '+bookRead)
         return (title+', by '+author+', '+pages+' pages, '+bookRead)
     }
 }
 
-const book1 = new Book('kama sutra', 'buddha', 690, 'not read yet')
+function addBookToLibrary() {
+    let newTitle = prompt("type title of book")
+    let newAuthor = prompt("type the author's full name")
+    let newpages = prompt("Enter the book's pages")
+    let newBookread = prompt("type the author's full name")
+    let newBook = new Book(newTitle, newAuthor, newpages, newBookread)
+    myLibrary.push(newBook)
+}
 
-console.log(book1.sayBookinfo())
+// const book1 = new Book('kama sutra', 'buddha', 690, 'not read yet')
+
+// console.log(book1.info())
+addBookToLibrary();
+console.log(myLibrary[0]);
