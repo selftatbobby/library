bd6dd567fc84372081b1507d0b1ee4b4156414d4
Bobby Tat <messaroundacc1@hotmail.com>
2022-12-06 04:44:07 -0800

    JS functions are operational. Buttons added to HTML and styled/organized with CSS

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 <!-- Next: work on changing console.log to return for the prototype function that reports the book information -->
 <!-- work on addBookToLibrary function -->
 <!-- work on showLibrary() function, it works so far... -->
-    work on looping through array
-    work on creating buttons
-    work on adding more books
+<!-- work on looping through array -->
+Work on creating buttons
+    Add a button on each book’s display to remove the book from the library.
+    Add a button on each book’s display to change its read status.
+<!-- work on adding more books -->
--- javascript.js
+++ javascript.js
@@ -12,25 +12,40 @@ function Book(title, author, pages, bookRead) {
 }
 
 function addBookToLibrary() {
-    let newTitle = prompt("type title of book")
-    let newAuthor = prompt("type the author's full name")
-    let newpages = prompt("Enter the book's pages")
-    let newBookread = prompt("type 'read or did not read'")
+    let newTitle = prompt("What's the title of book?")
+    let newAuthor = prompt("Provide author's full name:")
+    let newpages = prompt("How many pages?")
+    let newBookread = prompt("Did you 'read' or 'did not read'?")
     let newBook = new Book(newTitle, newAuthor, newpages, newBookread)
     myLibrary.push(newBook)
 }
 
+const book1 = new Book('kama sutra', 'buddha', 690, 'did not read')
+const book2 = new Book('bible', 'god', 690, 'did not read')
+const book3 = new Book('quran', 'allah', 690, 'did not read')
+const book4 = new Book('torah', 'yaweh', 690, 'did not read')
+myLibrary.push(book1);
+myLibrary.push(book2);
+myLibrary.push(book3);
+myLibrary.push(book4);
+
+
 function showLibrary () {
-    let bookCard = document.createElement('p');
-    bookCard.innerHTML = `I ${book1.bookRead} the book called ${book1.title}. It has ${book1.pages} pages. Authored by ${book1.author}.`;
-    document.getElementById("bookshelf").appendChild(bookCard); 
+    for (let i = 0; i < myLibrary.length; i++) {
+        let bookCard = document.createElement('div');
+        bookCard.innerHTML = `I ${myLibrary[i].bookRead} the book called ${myLibrary[i].title}. It has ${myLibrary[i].pages} pages. Authored by ${myLibrary[i].author}.`;
+        document.getElementById("bookshelf").appendChild(bookCard);
+    }   
 }
 
 
-const book1 = new Book('kama sutra', 'buddha', 690, 'did not read')
-showLibrary();
-// console.log(book1.info())
+/****************************Buttons**************************/
+let btn_element = document.getElementById("library_display");
+btn_element.addEventListener("click", () => {
+    showLibrary();
+});
 
-
-// addBookToLibrary();
-// console.log(myLibrary[0]);
+let btn_element2 = document.getElementById("add_book");
+btn_element2.addEventListener("click", () => {
+    addBookToLibrary();
+});
